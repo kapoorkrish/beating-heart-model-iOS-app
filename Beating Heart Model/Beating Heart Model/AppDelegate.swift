f49e776cbf55b61e789b18e72f32db1697958f73
Krish Kapoor <kapoorkrish26@gmail.com>
2022-12-27 21:26:18 -0500

    iOS app support with flask server!

--- Beating Heart Model/Beating Heart Model/AppDelegate.swift
+++ Beating Heart Model/Beating Heart Model/AppDelegate.swift
@@ -6,15 +6,12 @@
 //
 
 import UIKit
-import FirebaseCore
-import FirebaseDatabase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
         return true
     }
 
@@ -30,13 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-        setDataInt(NAME: "bpm", VAL: 0)
+        
+        //setDataInt(NAME: "bpm", VAL: 0)
     }
-    
-    func setDataInt(NAME: String, VAL: Int) {
-        let ref: DatabaseReference! = Database.database().reference()
-        ref.child(NAME).setValue(VAL)
-    }
-    
 }
 
--- Beating Heart Model/Beating Heart Model/ViewController.swift
+++ Beating Heart Model/Beating Heart Model/ViewController.swift
@@ -6,41 +6,40 @@
 //
 
 import UIKit
-import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var heartImage: UIImageView!
     @IBOutlet weak var heartButton: UIButton!
     @IBOutlet weak var bpm: UITextField!
     @IBOutlet weak var toggleIrregular: UISwitch!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var shutdownButton: UIButton!
     
     let heart_color_animation = [UIImage(named: "heart 1")!, UIImage(named: "heart 2")!, UIImage(named: "heart 3")!, UIImage(named: "heart 4")!, UIImage(named: "heart 5")!, UIImage(named: "heart 6")!, UIImage(named: "heart 7")!, UIImage(named: "heart 8")!, UIImage(named: "heart 9")!, UIImage(named: "heart 10")!]
+    
+    var data: [String : Any] = [
+        "bpm": 0,
+        "condition": "Regular",
+        "shutdown": "No"
+    ]
+    
+    var condition = "Regular"
+    var bpmVal = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
         heartImage.image = UIImage(named: "heart 1")
         heartImage.animationImages = heart_color_animation
         
-        setDataInt(NAME: "bpm", VAL: 0)
-        setDataStr(NAME: "condition", VAL: "Regular")
+        sendData(bpm: 0, condition: condition)
     }
-    
-    func setDataInt(NAME: String, VAL: Int) {
-        let ref: DatabaseReference! = Database.database().reference()
-        ref.child(NAME).setValue(VAL)
-    }
-    func setDataStr(NAME: String, VAL: String) {
-        let ref: DatabaseReference! = Database.database().reference()
-        ref.child(NAME).setValue(VAL)
-    }
-    
+
     @IBAction func sliderUsed(_ sender: Any) {
         
-        var bpmVal: Int = Int(bpm.text ?? "0") ?? 0
+        bpmVal = Int(bpm.text ?? "0") ?? 0
         bpmVal = Int(slider.value)
         bpm.text = "\(bpmVal)"
         
@@ -51,7 +50,8 @@ class ViewController: UIViewController {
                 heartImage.startAnimating()
                 heartImage.image = UIImage(named: "heart 10")
             }
-            setDataInt(NAME: "bpm", VAL: bpmVal)
+            sendData(bpm: bpmVal, condition: condition)
+
         }
         else {
             heartImage.image = UIImage(named: "heart 1")
@@ -59,9 +59,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func heartButtonClicked(_ sender: Any) {
+        
         if bpm.isFirstResponder && bpm.hasText {
             bpm.resignFirstResponder()
-            let bpmVal: Int = Int(bpm.text!)!
+            bpmVal = Int(bpm.text!)!
             slider.value = Float(bpmVal)
             
             if (bpmVal > 0 && bpmVal <= 200) {
@@ -71,7 +72,7 @@ class ViewController: UIViewController {
                     heartImage.startAnimating()
                     heartImage.image = UIImage(named: "heart 10")
                 }
-                setDataInt(NAME: "bpm", VAL: bpmVal)
+                sendData(bpm: bpmVal, condition: condition)
             }
             else {
                 heartImage.image = UIImage(named: "heart 1")
@@ -81,11 +82,52 @@ class ViewController: UIViewController {
     
     @IBAction func toggleIrregular(_ sender: Any) {
         if toggleIrregular.isOn {
-            setDataStr(NAME: "condition", VAL: "Irregular")
+            condition = "Irregular"
         }
         else {
-            setDataStr(NAME: "condition", VAL: "Regular")
+            condition = "Regular"
         }
+        sendData(bpm: bpmVal, condition: condition)
     }
     
+    
+    @IBAction func shutdownButtonClicked(_ sender: Any) {
+        shutdownButton.setBackgroundImage(UIImage(systemName: "power.circle"), for: .normal)
+        
+        sendData(bpm: 0, condition: "Regular", shutdown: "Yes")
+    }
+    
+    func api_post() {
+        let url = URL(string: "http://192.168.1.51:5000/database")
+        var request = URLRequest(url: url!)
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: data, options: .fragmentsAllowed)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if (data != nil && error == nil) {
+                print(data as Any)
+                print(response as Any)
+            }
+            else {
+                print("Error:", error as Any)
+            }
+        }
+        task.resume()
+    }
+    
+    func sendData(bpm: Int, condition: String, shutdown: String? = "No") {
+        data["bpm"] = bpm
+        data["condition"] = condition
+        data["shutdown"] = shutdown
+        api_post()
+        
+        print("BPM:", bpm)
+        print("Condition:", condition)
+        if shutdown == "Yes" {
+            print("Shutting down")
+        }
+    }
 }
